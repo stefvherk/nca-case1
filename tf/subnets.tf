35d9c4c056273de5ecd826a4bf34f1afda713949
Stef van Herk <547380@student.fontys.nl>
2025-09-18 13:19:48 +0200

    Fixed naming issue

--- tf/subnets.tf
+++ tf/subnets.tf
@@ -9,6 +9,13 @@ resource "aws_subnet" "subnet_hub" {
   }
 }
 
+resource "aws_subnet" "subnet_spoke_container_public" {
+  vpc_id                  = aws_vpc.spoke_container.id
+  cidr_block              = "172.16.2.0/28"
+  availability_zone       = "eu-central-1a"
+  map_public_ip_on_launch = true
+}
+
 resource "aws_subnet" "subnet_spoke_container_private" {
   vpc_id                  = aws_vpc.vpc_spoke_container.id
   cidr_block              = "172.16.1.0/28"
