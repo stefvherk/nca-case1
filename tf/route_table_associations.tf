ed545727f560e792159bcef73a95d15415d9d956
Stef van Herk <547380@student.fontys.nl>
2025-09-19 10:01:54 +0200

    Fixed subnetting issue

--- tf/route_table_associations.tf
+++ tf/route_table_associations.tf
@@ -1,3 +1,25 @@
+resource "aws_ec2_transit_gateway_route_table" "rt_egress" {
+  transit_gateway_id = aws_ec2_transit_gateway.tgw_hub.id
+
+  tags = {
+    Name = "TransitGatewayEgressRouteTable"
+  }
+}
+
+resource "aws_route_table" "rt_hub" {
+  vpc_id = aws_vpc.vpc_hub.id
+
+  tags = {
+    Name = "HubRouteTable"
+  }
+}
+
+resource "aws_route" "hub_to_nat_gateway" {
+  route_table_id         = aws_route_table.rt_hub.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_hub.id
+}
+
 resource "aws_route_table_association" "rta_hub" {
   subnet_id      = aws_subnet.subnet_hub.id
   route_table_id = aws_route_table.rt_hub.id
@@ -28,6 +50,15 @@ resource "aws_ec2_transit_gateway_route_table_association" "spoke_container_asso
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.rt_egress.id
 }
 
+resource "aws_ec2_transit_gateway_route_table_association" "spoke_data_assoc" {
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_spoke_data.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.rt_egress.id
+}
+
+resource "aws_ec2_transit_gateway_route_table_association" "spoke_monitoring_assoc" {
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_spoke_monitoring.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.rt_egress.id
+}
 resource "aws_ec2_transit_gateway_route_table_propagation" "propogation_spoke_container" {
   transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_spoke_container.id
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.rt_egress.id
