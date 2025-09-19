2aa89de9125efdd584186314ecc8da40b36e8c4f
Stef van Herk <547380@student.fontys.nl>
2025-09-19 09:42:16 +0200

    Fixed tgw naming issue

--- tf/transit_gateway.tf
+++ tf/transit_gateway.tf
@@ -8,19 +8,19 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_attachment_hub" {
   subnet_ids         = [aws_subnet.subnet_hub_private.id]
 }
 
-resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_attachment_spoke_container " {
+resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_attachment_spoke_container" {
   transit_gateway_id = aws_ec2_transit_gateway.tgw_hub.id
   vpc_id             = vpc_spoke_container.id
   subnet_ids         = [aws_subnet.subnet_spoke_container_private.id]
 }
 
-resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_attachment_spoke_data " {
+resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_attachment_spoke_data" {
   transit_gateway_id = aws_ec2_transit_gateway.tgw_hub.id
   vpc_id             = vpc_spoke_data.id
   subnet_ids         = [aws_subnet.subnet_spoke_data_private.id]
 }
 
-resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_attachment_spoke_monitoring " {
+resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_attachment_spoke_monitoring" {
   transit_gateway_id = aws_ec2_transit_gateway.tgw_hub.id
   vpc_id             = vpc_spoke_monitoring.id
   subnet_ids         = [aws_subnet.subnet_spoke_monitoring_private.id]
