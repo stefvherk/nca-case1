cc99ed7d0bc557a1c498217ff90317eda092272b
Stef van Herk <547380@student.fontys.nl>
2025-09-19 09:50:01 +0200

    Fixed incorrect referencing

--- tf/transit_gateway.tf
+++ tf/transit_gateway.tf
@@ -4,25 +4,25 @@ resource "aws_ec2_transit_gateway" "tgw_hub" {
 
 resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_attachment_hub" {
   transit_gateway_id = aws_ec2_transit_gateway.tgw_hub.id
-  vpc_id             = vpc_hub.id
+  vpc_id             = aws_vpc.vpc_hub.id
   subnet_ids         = [aws_subnet.subnet_hub_private.id]
 }
 
 resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_attachment_spoke_container" {
   transit_gateway_id = aws_ec2_transit_gateway.tgw_hub.id
-  vpc_id             = vpc_spoke_container.id
+  vpc_id             = aws_vpc.vpc_spoke_container.id
   subnet_ids         = [aws_subnet.subnet_spoke_container_private.id]
 }
 
 resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_attachment_spoke_data" {
   transit_gateway_id = aws_ec2_transit_gateway.tgw_hub.id
-  vpc_id             = vpc_spoke_data.id
+  vpc_id             = aws_vpc.vpc_spoke_data.id
   subnet_ids         = [aws_subnet.subnet_spoke_data_private.id]
 }
 
 resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_attachment_spoke_monitoring" {
   transit_gateway_id = aws_ec2_transit_gateway.tgw_hub.id
-  vpc_id             = vpc_spoke_monitoring.id
+  vpc_id             = aws_vpc.vpc_spoke_monitoring.id
   subnet_ids         = [aws_subnet.subnet_spoke_monitoring_private.id]
 }
 
